2c9b0bb76a713de40d6d9f3aab3538beb6d3c052
Melissa Adasme <adasme@ebi.ac.uk>
2025-09-05 14:10:01 +0100

    script 5 updated to formatted comments and regular spark session and writing

--- 2_probesLit/5_get_OT_dated_evidence.py
+++ 2_probesLit/5_get_OT_dated_evidence.py
@@ -1,6 +1,5 @@
-from tools import download_files_from_ftp
-import pyarrow.parquet as pq
-from tools import initialize_sparksession
+from tools import write_merged_files
+from pyspark.sql import SparkSession
 from pyspark.sql import DataFrame
 from pyspark.sql.functions import collect_list, when, col, size
 import pyspark.sql.functions as f
@@ -8,20 +7,38 @@ import argparse
 import os
 from functools import reduce
 
+# Define memory settings
+# Choose a value less than the --mem you request in SLURM (e.g., 120g for a 150g node)
+driver_mem = "200g"
+executor_mem = "200g"
+
+spark = SparkSession.builder \
+    .appName("Chemical Probes Lit Job") \
+    .config("spark.driver.memory", driver_mem) \
+    .config("spark.executor.memory", executor_mem) \
+    .config("spark.driver.memoryOverhead", "10g") \
+    .config("spark.driver.maxResultSize", "2g") \
+    .getOrCreate()
+
 
 def main(input_path, OTdatedevidence_path):
-    # Sparl session initialized with increased  Memory
-    spark = initialize_sparksession()
+    ## 1. Initialise Spark Session 
+    spark = SparkSession.builder.appName("Chemical Probes Lit Job").getOrCreate()
+    print("✅ Spark Session initialized.", flush=True)
 
-    ### PROBES dataset ###
+    ## 2. Load NER probes with evidence dataset
     probes_data = (
             spark.read.csv(input_path, sep="\t", header=True)
         )
+    print("✅ Loaded NER probes with evidence from step 4.", flush=True)
 
-    # Get a list of all subfolders
+    ## 3. Get a list of all subfolders
     data_folder = OTdatedevidence_path # path of the main folder containing al subfolders for each data type
     subfolders = [f.path for f in os.scandir(data_folder) if f.is_dir()]
+    print("✅ Subfolders retrieved.", flush=True)
 
+
+    ## 4. obtained evidence with date for all subfolders
     # Create an empty list to store the results
     probes_dataed_list = []
 
@@ -50,8 +67,9 @@ def main(input_path, OTdatedevidence_path):
 
     # Combine all dataframes in the list
     probes_dataev = reduce(DataFrame.unionAll, probes_dataed_list) 
+    print("✅ All evidence combined.", flush=True)
 
-    # Perform the join on both diseaseId and targetId
+    ## 5. Link probes taregt-disease pairs with dated evidence. Perform the join on both diseaseId and targetId
     #probes_dataed = probes_dataev.join(probes_data, ["diseaseId", "targetId", "datasourceId"], "right_outer")
 
     probes_dataed = (
@@ -67,16 +85,15 @@ def main(input_path, OTdatedevidence_path):
         .withColumn("allpublicationYears", when(size(col("allpublicationYears")) == 0, None).otherwise(col("allpublicationYears")))
         .withColumn("allstudyYears", when(size(col("allstudyYears")) == 0, None).otherwise(col("allstudyYears")))
     )
+    print("✅ All dated evidence linked.", flush=True)
 
-    # Add the probes_data with NULL datasourceId to the final result
-    probes_dataed.show(5)
-
-    # Save the results
-    #probes_dataed.toPandas().to_csv("data/ner_probes_triplets_ptpairs_evd.csv", sep="\t", index=False)
-    probes_dataed.show(5)
+    ## 6. Create the file with the results
     probes_dataed.toPandas().to_csv("data/ner_probes_triplets_ptpairs_evd.csv", sep="\t", index=False)
-    print(probes_data.count())
-    print(probes_dataed.count())
+    ## 5. Writing output file
+    temp_output_dir = "temp/evidence_output_parts"
+    final_output_dir = "data/5_ner_probes_triplets_ptpairs_evd.tsv"
+    write_merged_files(probes_dataed, temp_output_dir, final_output_dir) #function from tools.py
+    print("✅ File with OT dated evidence linked to chemical probes data created", flush=True)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Find all OT evidence for each T-D from the NER chemical probes literature data.') 
